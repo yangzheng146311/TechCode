176c437d08295f27e5ddac427e3ce0b8c1da919a
Zheng Yang <b8047712@newcastle.ac.uk>
2018-11-28 21:49:08 +0000

    Linear motion

--- CSC8503/CSC8503Common/PhysicsSystem.cpp
+++ CSC8503/CSC8503Common/PhysicsSystem.cpp
@@ -176,7 +176,24 @@ based on any forces that have been accumulated in the objects during
 the course of the previous game frame.
 */
 void PhysicsSystem::IntegrateAccel(float dt) {
-
+	std::vector <GameObject*>::const_iterator first;
+	std::vector <GameObject*>::const_iterator last; 
+	gameWorld.GetObjectIterators(first, last); 
+	for (auto i = first; i != last; ++i) {
+		PhysicsObject* object = (*i)->GetPhysicsObject(); 
+		if (object == nullptr) {
+			 continue; //No physics object for this GameObject!
+		} 
+		float inverseMass = object ->GetInverseMass();
+		Vector3 linearVel = object ->GetLinearVelocity(); 
+		Vector3 force = object ->GetForce(); 
+		Vector3 accel = force * inverseMass; 
+		if (applyGravity && inverseMass > 0) { 
+			accel += gravity; //don’t move infinitely heavy things 
+		} 
+		linearVel += accel * dt; //integrate accel! 
+		object ->SetLinearVelocity(linearVel); 
+	} 
 }
 /*
 This function integrates linear and angular velocity into
@@ -185,7 +202,27 @@ throughout a physics update, to slowly move the objects through
 the world, looking for collisions.
 */
 void PhysicsSystem::IntegrateVelocity(float dt) {
-
+	std::vector <GameObject*>::const_iterator first;
+	std::vector <GameObject*>::const_iterator last; 
+	gameWorld.GetObjectIterators(first, last);
+	float dampingFactor = 1.0f - 0.95f; 
+	float frameDamping = powf(dampingFactor, dt);
+	for (auto i = first; i != last; ++i) {
+		
+		PhysicsObject* object = (*i)->GetPhysicsObject();
+		if (object == nullptr) { 
+			continue; 
+		} 
+		Transform&transform = (*i)->GetTransform();
+		//Position Stuff 
+		Vector3 position = transform.GetLocalPosition();
+		Vector3 linearVel = object ->GetLinearVelocity(); 
+		position += linearVel * dt;
+		transform.SetLocalPosition(position); 
+		// Linear Damping 
+		linearVel = linearVel * frameDamping; 
+		object ->SetLinearVelocity(linearVel); 
+	} 
 }
 
 /*
